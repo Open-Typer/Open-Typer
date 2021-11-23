485ffbc48eca2215a0635efd40cbc7197610c0eb
adazem009 <68537469+adazem009@users.noreply.github.com>
2021-11-23 21:56:36 +0100

    core: configfile: Mark old repeat type function deprecated

--- src/include/core/configfile.h
+++ src/include/core/configfile.h
@@ -54,7 +54,7 @@ int _lesson_count(FILE *cr) __attribute__ ((deprecated));
 int _lesson_sublesson_count(FILE *cr, int tlesson) __attribute__ ((deprecated));
 int _lesson_sublesson_level_count(FILE *cr, int tlesson, int tsublesson) __attribute__ ((deprecated));
 int _lesson_sublesson_level_line(FILE *cr, int tlesson, int tsublesson, int tlevel) __attribute__ ((deprecated));
-char* _lesson_sublesson_level_repeat_type(FILE *cr, int tlesson, int tsublesson, int tlevel);
+char* _lesson_sublesson_level_repeat_type(FILE *cr, int tlesson, int tsublesson, int tlevel) __attribute__ ((deprecated));
 char *_lesson_desc(FILE *cr, int tlesson);
 int _lesson_sublesson_level_limit_extension(FILE *cr, int tlesson, int tsublesson, int tlevel);
 int _lesson_sublesson_level_length_extension(FILE *cr, int tlesson, int tsublesson, int tlevel);
