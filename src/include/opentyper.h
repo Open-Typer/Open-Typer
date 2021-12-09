fc3f36750f2d7ba5d7563a824ccbb06489fc9c9d
adazem009 <68537469+adazem009@users.noreply.github.com>
2021-12-09 16:16:21 +0100

    opentyper: Remove obsolete autoUpdate() member

--- src/include/opentyper.h
+++ src/include/opentyper.h
@@ -78,7 +78,6 @@ private:
 	int lastTime;
 	int _line_count(QString str);
 	Updater *versionHdw;
-	void autoUpdate(void);
 	void adjustSize(void);
 	int labelWidth(QLabel *targetLabel, QString labelText);
 	void setFont(QString fontFamily, int fontSize, bool fontBold, bool fontItalic, bool fontUnderline);
