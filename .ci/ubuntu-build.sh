8c9c74c6fd8ed1ecd4f281e36877cc80c87f1019
adazem009 <68537469+adazem009@users.noreply.github.com>
2022-01-09 14:27:43 +0100

    CI: Disable deb build

--- .ci/ubuntu-build.sh
+++ .ci/ubuntu-build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# TODO: Rewrite deb build script
-#.ci/common/build.sh linux
-git config --local user.name "adazem009"
-git config --local user.email "68537469+adazem009@users.noreply.github.com"
-./build-deb.sh
+.ci/common/build.sh linux
