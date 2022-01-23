029c40dec58aa263d90ff8ca86e47bbf742d7147
adazem009 <68537469+adazem009@users.noreply.github.com>
2022-01-23 19:33:24 +0100

    Remove old updater part

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
  * main.cpp
  * This file is part of Open-Typer
  *
- * Copyright (C) 2021 - adazem009
+ * Copyright (C) 2021-2022 - adazem009
  *
  * Open-Typer is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,15 +27,6 @@ int main(int argc, char *argv[])
 {
 	QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 	QApplication a(argc, argv);
-	// Check if the program was recently updated
-	if(argc > 1)
-	{
-		QFile::remove(QString(argv[1]));
-		QFile::rename(QString(argv[1])+".part",QString(argv[1]));
-		QMessageBox updateSuccess;
-		updateSuccess.setText(QObject::tr("Successfully updated."));
-		updateSuccess.exec();
-	}
 	// Set icon
 	a.setWindowIcon(QIcon(":/res/images/icon.ico"));
 	OpenTyper w;
