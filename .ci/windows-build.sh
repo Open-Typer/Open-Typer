574b8f01658bdf6d93f58a6e9a68f80993521689
adazem009 <68537469+adazem009@users.noreply.github.com>
2022-06-04 19:18:13 +0200

    CI: Windows: Fix order of commands

--- .ci/windows-build.sh
+++ .ci/windows-build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-source .ci/common/build.sh win32
 curl -o openssl.zip https://github.com/CristiFati/Prebuilt-Binaries/raw/master/OpenSSL/v1.1.1/OpenSSL-1.1.1f-Win-pc032.zip
 mkdir C:/OpenSSL
 unzip openssl.zip -d ../openssl
 mv ../openssl/* C:/OpenSSL
 rm openssl.zip
 find C:/OpenSSL/lib -depth -name "*.lib" -exec sh -c 'f="{}"; mv -- "$f" "${f%.lib}.a"' \;
+source .ci/common/build.sh win32
 cd release
 rm -f *.o *.cpp *.h
 windeployqt Open-Typer.exe
