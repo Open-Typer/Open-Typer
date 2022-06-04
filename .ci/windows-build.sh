aa03413a636bc9817d4bf1bbd17d6754a8c82fb9
adazem009 <68537469+adazem009@users.noreply.github.com>
2022-06-04 19:04:23 +0200

    CI: Windows: Fix OpenSSL lib file names

--- .ci/windows-build.sh
+++ .ci/windows-build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 source .ci/common/build.sh win32
-
-cd release
-rm -f *.o *.cpp *.h
-windeployqt Open-Typer.exe
 curl -o openssl.zip https://github.com/CristiFati/Prebuilt-Binaries/raw/master/OpenSSL/v1.1.1/OpenSSL-1.1.1f-Win-pc032.zip
 mkdir C:/OpenSSL
 unzip openssl.zip -d ../openssl
 mv ../openssl/* C:/OpenSSL
 rm openssl.zip
+find C:/OpenSSL/lib -depth -name "*.lib" -exec sh -c 'f="{}"; mv -- "$f" "${f%.lib}.a"' \;
+cd release
+rm -f *.o *.cpp *.h
+windeployqt Open-Typer.exe
 cp C:/OpenSSL/bin/*.dll ./
 
 # Installer - does not work in CI right now...
