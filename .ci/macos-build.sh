f4f028f0e766769c910d3f0047941b733ba5c8a2
adazem009 <68537469+adazem009@users.noreply.github.com>
2022-07-01 23:00:06 +0200

    CI: MacOS: Find OpenSSL library files

--- .ci/macos-build.sh
+++ .ci/macos-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export CPATH=$CPATH:`echo /usr/local/Cellar/openssl@1.1/1.1.1*/include`
-.ci/common/build.sh macos
+export LIBRARY_PATH=`echo /usr/local/Cellar/openssl@1.1/1.1.1*/lib`
+. .ci/common/build.sh macos
 
 macdeployqt Open-Typer.app -dmg
