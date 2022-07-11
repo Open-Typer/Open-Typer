b8fc1451b7fd91be1d0cabb709b3e83efba2a1a8
adazem009 <68537469+adazem009@users.noreply.github.com>
2022-07-11 22:45:18 +0200

    CI: Remove linuxdeployqt after building AppImage

--- .ci/ubuntu-build.sh
+++ .ci/ubuntu-build.sh
@@ -15,4 +15,5 @@ mkdir -p AppDir/usr/bin &&
 mkdir -p AppDir/usr/lib &&
 cp -r res/linux-release/usr/share AppDir/usr/share &&
 cp open-typer AppDir/usr/bin/
-./linuxdeployqt-continuous-x86_64.AppImage AppDir/usr/share/applications/open-typer.desktop -appimage
+./linuxdeployqt-continuous-x86_64.AppImage AppDir/usr/share/applications/open-typer.desktop -appimage &&
+rm linuxdeployqt-continuous-x86_64.AppImage
