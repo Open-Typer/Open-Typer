ce86d5fc4ce94df8eafc831b14ddbd111af937f8
adazem009 <68537469+adazem009@users.noreply.github.com>
2022-07-11 22:42:19 +0200

    CI: Build AppImage

--- .ci/ubuntu-build.sh
+++ .ci/ubuntu-build.sh
@@ -6,4 +6,13 @@ sudo mk-build-deps --install debian/control &&
 
 # Build
 export QT_SELECT=qt5
-.ci/common/build.sh linux
+.ci/common/build.sh linux &&
+
+# Build AppImage
+wget https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage &&
+chmod +x linuxdeployqt-continuous-x86_64.AppImage &&
+mkdir -p AppDir/usr/bin &&
+mkdir -p AppDir/usr/lib &&
+cp -r res/linux-release/usr/share AppDir/usr/share &&
+cp open-typer AppDir/usr/bin/
+./linuxdeployqt-continuous-x86_64.AppImage AppDir/usr/share/applications/open-typer.desktop -appimage
