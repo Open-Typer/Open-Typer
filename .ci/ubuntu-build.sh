40878f5665b9edb52fcf3bd79cb71473984de49a
adazem009 <68537469+adazem009@users.noreply.github.com>
2022-07-11 22:41:12 +0200

    CI: Ubuntu: Add custom Qt installation steps

--- .ci/ubuntu-build.sh
+++ .ci/ubuntu-build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# Install Qt (use build dependencies from debian/control)
+sudo apt install -y devscripts equivs &&
+sudo mk-build-deps --install debian/control &&
+
+# Build
+export QT_SELECT=qt5
 .ci/common/build.sh linux
