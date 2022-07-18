711ee55d8b5f0047fdcc84663e0bf099b3e122f2
adazem009 <68537469+adazem009@users.noreply.github.com>
2022-07-18 16:05:21 +0200

    CI: Wait until wget succeeds in PPA download script

--- .ci/ubuntu-ppa-download.sh
+++ .ci/ubuntu-ppa-download.sh
@@ -3,4 +3,5 @@
 ### This script downloads new Ubuntu packages from the PPA
 VERSION=`git describe --tags --abbrev=0`
 VERSION=${VERSION//v}
-./.ci/common/get-package-urls.py $VERSION | xargs wget
+(./.ci/common/get-package-urls.py $VERSION | xargs wget) ||
+(sleep 10 && rm -f *.deb && exec "$0")
