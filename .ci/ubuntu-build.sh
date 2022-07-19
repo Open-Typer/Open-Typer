a38ee1fb89b1893e8fd481661f499b22cab917ef
adazem009 <68537469+adazem009@users.noreply.github.com>
2022-07-19 11:10:29 +0200

    CI: Ubuntu: Create zsync file for AppImage updates

--- .ci/ubuntu-build.sh
+++ .ci/ubuntu-build.sh
@@ -10,11 +10,12 @@ export QT_SELECT=qt5
 .ci/common/build.sh linux &&
 
 # Build AppImage
+sudo apt install -y zsync
 wget https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage &&
 chmod +x linuxdeployqt-continuous-x86_64.AppImage &&
 mkdir -p AppDir/usr/bin &&
 mkdir -p AppDir/usr/lib &&
 cp -r res/linux-release/usr/share AppDir/usr/share &&
 cp open-typer AppDir/usr/bin/
-./linuxdeployqt-continuous-x86_64.AppImage AppDir/usr/share/applications/open-typer.desktop -appimage &&
+./linuxdeployqt-continuous-x86_64.AppImage AppDir/usr/share/applications/open-typer.desktop -updateinformation="zsync|https://github.com/Open-Typer/Open-Typer/releases/latest/download/Open-Typer-x86_64.AppImage.zsync" -appimage &&
 rm linuxdeployqt-continuous-x86_64.AppImage
