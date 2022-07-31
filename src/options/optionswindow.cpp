252233b4acaf174a0872f578d41808ff4b589075
adazem009 <68537469+adazem009@users.noreply.github.com>
2022-07-31 12:34:05 +0200

    Add copyright info from commit 0803526
    * 8035260d4a288b1a24a555c6821e343992b7e72

--- src/options/optionswindow.cpp
+++ src/options/optionswindow.cpp
@@ -3,6 +3,7 @@
  * This file is part of Open-Typer
  *
  * Copyright (C) 2021-2022 - adazem009
+ * Copyright (C) 2022 - yeti0904
  *
  * Open-Typer is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
