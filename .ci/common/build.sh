47541e86bc2dfb5cccb8ffec38f82f9283e43926
adazem009 <68537469+adazem009@users.noreply.github.com>
2022-10-06 15:11:45 +0200

    CI: Properly handle build errors

--- .ci/common/build.sh
+++ .ci/common/build.sh
@@ -2,14 +2,19 @@
 
 PLATFORM=$1
 
-lrelease *.pro
-qmake
+# exit codes:
+# 1: unsupported platform
+# 2: lrelease failed
+# 3: qmake failed
+# 4: build (make) failed
+lrelease *.pro || exit 2
+qmake || exit 3
 if [[ "$PLATFORM" == "win32" ]]; then
-	mingw32-make.exe -j4
+	mingw32-make.exe -j4 || exit 4
 elif [[ "$PLATFORM" == "win64" ]]; then
-	mingw32-make.exe -j4
+	mingw32-make.exe -j4 || exit 4
 elif [[ "$PLATFORM" == "macos" ]]; then
-	make -j6
+	make -j6 || exit 4
 else
-	make -j$(nproc --all)
+	make -j$(nproc --all) || exit 4
 fi
