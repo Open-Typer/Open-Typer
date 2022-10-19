cfefc8ea82ff4860f025a5322fe54586b71ba59f
adazem009 <68537469+adazem009@users.noreply.github.com>
2022-10-19 18:39:46 +0200

    Do not use bash for Windows builds

--- .ci/windows-build.sh
+++ .ci/windows-build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-curl -o openssl.zip -LJO https://github.com/CristiFati/Prebuilt-Binaries/raw/master/OpenSSL/v1.1.1/OpenSSL-1.1.1q-Win-pc064.zip
-mkdir ../openssl
-unzip openssl.zip -d ../openssl
-mv ../openssl/OpenSSL/OpenSSL/* C:/OpenSSL
-rm openssl.zip
-find C:/OpenSSL/lib -depth -name "*.lib" -exec sh -c 'f="{}"; mv -- "$f" "${f%.lib}.a"' \;
-source .ci/common/build.sh win64
-cd release
-rm -f *.o *.cpp *.h
-windeployqt ${executable_name}.exe
-cp C:/OpenSSL/bin/*.dll ./
-cd ..
-mv release ../
+if [[ "$1" != "1" ]]; then
+	curl -o openssl.zip -LJO https://github.com/CristiFati/Prebuilt-Binaries/raw/master/OpenSSL/v1.1.1/OpenSSL-1.1.1q-Win-pc064.zip
+	mkdir ../openssl
+	unzip openssl.zip -d ../openssl
+	mv ../openssl/OpenSSL/OpenSSL/* C:/OpenSSL
+	rm openssl.zip
+	find C:/OpenSSL/lib -depth -name "*.lib" -exec sh -c 'f="{}"; mv -- "$f" "${f%.lib}.a"' \;
+else
+	cd release
+	rm -f *.o *.cpp *.h
+	cp C:/OpenSSL/bin/*.dll ./
+	cd ..
+	mv release ../
+fi
