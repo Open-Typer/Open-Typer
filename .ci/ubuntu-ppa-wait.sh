653a32b6d5dbc918d32f4cdb73730c267791cceb
adazem009 <68537469+adazem009@users.noreply.github.com>
2022-11-29 21:02:56 +0100

    CI: Print debug info in PPA wait script

--- .ci/ubuntu-ppa-wait.sh
+++ .ci/ubuntu-ppa-wait.sh
@@ -3,6 +3,11 @@
 ### This script waits until the new release is available in the PPA for Ubuntu
 VERSION=`git describe --tags --abbrev=0`
 VERSION=${VERSION//v}
+echo VERSION=$VERSION
+echo launchpad_name=$launchpad_name
+echo ppa_name=$ppa_name
+echo ppa_package_name=$ppa_package_name
+echo $(git for-each-ref --sort=-creatordate --count 1 --format="%(refname:short)" "refs/tags/*")
 while true; do
 	./.ci/common/get-build-status.py $launchpad_name $ppa_name $ppa_package_name $VERSION
 	code=$?
