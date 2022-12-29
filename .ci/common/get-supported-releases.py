ae772857cf59f93afbce77997f819418f7650277
adazem009 <68537469+adazem009@users.noreply.github.com>
2022-12-29 18:24:31 +0100

    CI: Support Ubuntu extended LTS since 18.04

--- .ci/common/get-supported-releases.py
+++ .ci/common/get-supported-releases.py
@@ -5,10 +5,8 @@ from launchpadlib.launchpad import Launchpad
 from datetime import date
 
 ### This Python script prints currently supported Ubuntu versions
-### Note: it isn't 100 % accurate, especially on LTS releases
 launchpad = Launchpad.login_anonymously('get-supported-releases', 'production') # login
-currentYear = date.today().year
 for series in launchpad.projects['ubuntu'].series:
-	# Launchpad seems to support LTS releases after their EOL, so at least compare the years
-	if series.supported and currentYear - series.datereleased.year <= 5:
+	# We support LTS releases with extended support, but only since Ubuntu 18.04 LTS
+	if series.supported and series.datereleased.year >= 2018:
 		print(series.name)
