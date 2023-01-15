0a7453f34615c4beafd7e4a90701e6e2b361ec1e
adazem009 <68537469+adazem009@users.noreply.github.com>
2023-01-15 19:36:31 +0100

    CI: Build linuxdeployqt from custom source

--- .ci/ubuntu-build.sh
+++ .ci/ubuntu-build.sh
@@ -3,10 +3,20 @@
 # Build
 .ci/common/build.sh linux &&
 
+# Build linuxdeployqt
+sudo apt install -y zsync patchelf desktop-file-utils
+git clone https://github.com/Open-Typer/linuxdeployqt linuxdeployqt-build
+cd linuxdeployqt-build
+qmake
+make -j$(nproc --all)
+mv bin/linuxdeployqt ..
+cd ..
+rm -rf linuxdeployqt-build
+wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
+chmod +x appimagetool*.AppImage
+sudo mv appimagetool*.AppImage /usr/bin/appimagetool
+
 # Build AppImage
-sudo apt install -y zsync
-wget https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage &&
-chmod +x linuxdeployqt-continuous-x86_64.AppImage &&
 mkdir -p AppDir/usr/bin &&
 mkdir -p AppDir/usr/lib &&
 mkdir -p AppDir/usr/share/applications &&
@@ -14,5 +24,5 @@ cp -r app/res/linux-release/usr/share/pixmaps AppDir/usr/share/ &&
 cp app/res/linux-release/usr/share/applications/open-typer-appimage.desktop AppDir/usr/share/applications/open-typer.desktop &&
 cp "$executable_name" AppDir/usr/bin/
 cp *.so* AppDir/usr/lib/
-./linuxdeployqt-continuous-x86_64.AppImage AppDir/usr/share/applications/*.desktop -qmldir=app/ui/qml -updateinformation="zsync|${appimage_zsync_url}" -appimage &&
-rm linuxdeployqt-continuous-x86_64.AppImage
+./linuxdeployqt AppDir/usr/share/applications/*.desktop -qmldir=app/ui/qml -updateinformation="zsync|${appimage_zsync_url}" -appimage &&
+rm linuxdeployqt
