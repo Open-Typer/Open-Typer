17e63df94dacd2c191e468eecc91fc8868bb3682
adazem009 <68537469+adazem009@users.noreply.github.com>
2023-02-13 18:40:20 +0100

    ExerciseValidator: Use qreal in time property

--- libcore/src/ExerciseValidator.h
+++ libcore/src/ExerciseValidator.h
@@ -40,7 +40,7 @@ class CORE_LIB_EXPORT ExerciseValidator : public QObject
 		Q_PROPERTY(QList<MistakeRecord> mistakes READ mistakes WRITE setMistakes NOTIFY mistakesChanged)
 		Q_PROPERTY(QList<CharacterRecord> characters READ characters WRITE setCharacters NOTIFY charactersChanged)
 		Q_PROPERTY(bool timed READ isTimed WRITE setTimed NOTIFY timedChanged)
-		Q_PROPERTY(int time READ time WRITE setTime NOTIFY timeChanged)
+		Q_PROPERTY(qreal time READ time WRITE setTime NOTIFY timeChanged)
 	public:
 		void setExerciseText(QString text);
 		QString exerciseText(void);
