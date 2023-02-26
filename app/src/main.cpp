3c5d3c1392cce1fdd5c2b653d3d188eb97f68302
Roker2 <dimaminko2000@gmail.com>
2023-02-26 08:28:04 +0100

    Update credits

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -3,7 +3,7 @@
  * This file is part of Open-Typer
  *
  * Copyright (C) 2021-2023 - adazem009
- * Copyright (C) 2022 - Roker2
+ * Copyright (C) 2022-2023 - Roker2
  *
  * Open-Typer is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
