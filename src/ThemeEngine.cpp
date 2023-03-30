afc8fc0f19f9d252f55437d378e8a11d75e75408
adazem009 <68537469+adazem009@users.noreply.github.com>
2023-03-30 10:10:04 +0200

    ThemeEngine: Ignore italic and underline style of font
    Fixes #34

--- src/ThemeEngine.cpp
+++ src/ThemeEngine.cpp
@@ -45,7 +45,7 @@ ThemeEngine::ThemeEngine(QObject *parent) :
 /*! The selected font. */
 QFont ThemeEngine::font(void)
 {
-	if(!Settings::containsThemeFont() || !Settings::containsThemeFontSize() || !Settings::containsThemeFontBold() || !Settings::containsThemeFontItalic() || !Settings::containsThemeFontUnderline())
+	if(!Settings::containsThemeFont() || !Settings::containsThemeFontSize() || !Settings::containsThemeFontBold())
 	{
 		setFontFamily("");
 		setFontSize(20);
