36f4cff8d21554cfa65758a852c663ff6973fa21
adazem009 <68537469+adazem009@users.noreply.github.com>
2023-07-30 11:05:09 +0200

    Support Ubuntu 22.04 or later in PPA packages

--- .ci/common/get-supported-releases.py
+++ .ci/common/get-supported-releases.py
@@ -7,6 +7,6 @@ from datetime import date
 ### This Python script prints currently supported Ubuntu versions
 launchpad = Launchpad.login_anonymously('get-supported-releases', 'production') # login
 for series in launchpad.projects['ubuntu'].series:
-	# Support Ubuntu 20.04 or later
-	if series.supported and series.datereleased.year >= 2020:
+	# Support Ubuntu 22.04 or later
+	if series.supported and series.datereleased.year >= 2022:
 		print(series.name)
