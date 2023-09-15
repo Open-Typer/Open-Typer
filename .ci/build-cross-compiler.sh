df309a1726353ede6171465b935b42a95d7892e6
adazem009 <68537469+adazem009@users.noreply.github.com>
2023-09-15 20:14:24 +0200

    Use sysroot for custom cross compiler

--- .ci/build-cross-compiler.sh
+++ .ci/build-cross-compiler.sh
@@ -10,6 +10,9 @@ target_arch="$1"
 
 sudo apt install -y sed flex bison gzip gettext texinfo libelf-dev libgomp1 make tar libgmp-dev libmpfr-dev libmpc-dev libisl-dev build-essential || exit $?
 
+echo "Preparing sysroot..."
+sysroot_path=$(.ci/prepare-sysroot.sh "$target_arch")
+
 #git clone https://github.com/Open-Typer/gcc-cross-compiler || exit $?
 git init gcc-cross-compiler || exit $?
 cd gcc-cross-compiler
@@ -19,12 +22,12 @@ git checkout master || exit $?
 
 case "$target_arch" in
     aarch64)
-        sudo ./toolchain.py --arch aarch64 --install yes --cores $(nproc --all) || exit $?
+        sudo ./toolchain.py --arch aarch64 --install yes --cores $(nproc --all) --sysroot "$sysroot_path" || exit $?
         update-alternatives --install /usr/bin/aarch64-linux-gnu-gcc aarch64-linux-gnu-gcc /usr/local/cross/aarch64/bin/aarch64-linux-gnu-gcc 10 || exit $?
         update-alternatives --install /usr/bin/aarch64-linux-gnu-g++ aarch64-linux-gnu-g++ /usr/local/cross/aarch64/bin/aarch64-linux-gnu-g++ 10 || exit $?
         ;;
     armv7)
-        sudo ./toolchain.py --arch armhf --install yes --cores $(nproc --all) || exit $?
+        sudo ./toolchain.py --arch armhf --install yes --cores $(nproc --all) --sysroot "$sysroot_path" || exit $?
         update-alternatives --install /usr/bin/arm-linux-gnueabihf-gcc arm-linux-gnueabihf-gcc /usr/local/cross/armhf/bin/arm-linux-gnueabihf-gcc 10 || exit $?
         update-alternatives --install /usr/bin/arm-linux-gnueabihf-g++ arm-linux-gnueabihf-g++ /usr/local/cross/armhf/bin/arm-linux-gnueabihf-g++ 10 || exit $?
         ;;
