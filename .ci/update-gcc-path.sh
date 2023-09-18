3fad2fe6d6dee6acd874389eb3f77bca127b6c7d
adazem009 <68537469+adazem009@users.noreply.github.com>
2023-09-18 16:52:35 +0200

    Use gcc 11 for Linux build

--- .ci/update-gcc-path.sh
+++ .ci/update-gcc-path.sh
@@ -6,11 +6,11 @@ sudo ()
     "$@"
 }
 
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 9
-sudo update-alternatives --install /usr/bin/cc cc /usr/bin/gcc-9 9
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 9
-sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++-9 9
-sudo update-alternatives --set gcc /usr/bin/gcc-9
-sudo update-alternatives --set cc /usr/bin/gcc-9
-sudo update-alternatives --set g++ /usr/bin/g++-9
-sudo update-alternatives --set c++ /usr/bin/g++-9
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 9
+sudo update-alternatives --install /usr/bin/cc cc /usr/bin/gcc-11 9
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-11 9
+sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++-11 9
+sudo update-alternatives --set gcc /usr/bin/gcc-11
+sudo update-alternatives --set cc /usr/bin/gcc-11
+sudo update-alternatives --set g++ /usr/bin/g++-11
+sudo update-alternatives --set c++ /usr/bin/g++-11
